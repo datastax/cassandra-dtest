fe78fdb9783e9d867110bffaeb8e1a419fcc5d57
Branimir Lambov <branimir.lambov@datastax.com>
2021-04-12 11:56:25 +0300

    STAR-14: Fix expectations to include memtable table parameter
    (cherry picked from commit 353b1f13362cf3081bf908f636b0c886100e98f1)
    (cherry picked from commit 1cbc8d5b4a61c564a4c334b357cba3b48a44eac6)
    (cherry picked from commit ee9b61242476135852e38df8cb18b1adf25dc73d)
    (cherry picked from commit e3e226a78f4bcc4dd33aa10c099415f25994547e)
    (cherry picked from commit b52785321ab06fcbe13ade67ed8f61c40ad75d19)
    (cherry picked from commit 581f69b74beb04a61bf9b49414cedcec76eec7e3)
    (cherry picked from commit d0482217c13d103c37794ead82fe721391b1708a)

--- cqlsh_tests/test_cqlsh.py
+++ cqlsh_tests/test_cqlsh.py
@@ -1183,6 +1183,7 @@ CREATE TYPE test.address_type (
             AND comment = ''
             AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
             AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+            AND memtable = {}
             AND crc_check_chance = 1.0
             AND default_time_to_live = 0
             AND extensions = {}
@@ -1323,6 +1324,7 @@ CREATE TYPE test.address_type (
             AND comment = ''
             AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
             AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+            AND memtable = {}
             AND crc_check_chance = 1.0
             AND default_time_to_live = 0
             AND extensions = {}
@@ -1481,6 +1483,7 @@ CREATE TYPE test.address_type (
                 AND comment = ''
                 AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
                 AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+                AND memtable = {}
                 AND crc_check_chance = 1.0
                 AND extensions = {}
                 AND gc_grace_seconds = 864000
