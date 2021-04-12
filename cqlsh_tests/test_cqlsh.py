ee9b61242476135852e38df8cb18b1adf25dc73d
Branimir Lambov <branimir.lambov@datastax.com>
2021-04-12 11:56:25 +0300

    STAR-14: Fix expectations to include memtable table parameter
    (cherry picked from commit 353b1f13362cf3081bf908f636b0c886100e98f1)
    (cherry picked from commit 1cbc8d5b4a61c564a4c334b357cba3b48a44eac6)

--- cqlsh_tests/test_cqlsh.py
+++ cqlsh_tests/test_cqlsh.py
@@ -1103,6 +1103,7 @@ CREATE TYPE test.address_type (
             AND comment = ''
             AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
             AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+            AND memtable = {}
             AND crc_check_chance = 1.0
             AND default_time_to_live = 0
             AND extensions = {}
@@ -1192,6 +1193,7 @@ CREATE TYPE test.address_type (
             AND comment = ''
             AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
             AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+            AND memtable = {}
             AND crc_check_chance = 1.0
             AND default_time_to_live = 0
             AND extensions = {}
@@ -1298,6 +1300,7 @@ CREATE TYPE test.address_type (
                 AND comment = ''
                 AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
                 AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+                AND memtable = {}
                 AND crc_check_chance = 1.0
                 AND default_time_to_live = 0
                 AND extensions = {}
