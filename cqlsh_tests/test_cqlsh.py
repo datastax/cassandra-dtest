e83d99a9e72dbe968af8dc25bf23c31571b27cb6
Tomek Łasica <tlasica@life.pl>
2021-06-16 12:59:43 +0200

    STAR-765: Add tests for cloud connection. (#23)
    * STAR-765: Add tests for cloud connection.
    
    It is not possible without some infrastructre or tooling effort to trully test cloud connection.
    Instead added tests focus on proper cqlsh behavior when secure connect bundle is specified:
    - proper default consistency level
    - debug information
    - reading from cqlshrc and parameters
    - skipping host / port information
    
    Testing validation is based on error msgs and debug info.
    
    (cherry picked from commit 9f8cba36d6bfbed92f9df0ccd86d8048d845d59b)
    (cherry picked from commit 50bf8511799fc7f97d9f47a873630fef0c317108)

--- cqlsh_tests/test_cqlsh.py
+++ cqlsh_tests/test_cqlsh.py
@@ -93,6 +93,7 @@ class CqlshMixin():
             logger.debug("Cqlsh command stderr:\n" + stderr)
         return stdout, stderr
 
+
 class TestCqlsh(Tester, CqlshMixin):
 
     # override cluster options to enable user defined functions
