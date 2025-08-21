dc33384f1c2f366c9f3a11c2d546e34eb02ebb8a
mck <mck@apache.org>
2025-08-21 23:22:04 +0200

    CNDB-15022 Introduce additional guardrail profile for HCD
    There are now three profiles for guardrail defaults: none|dbaas|hcd
    
    Tests on transient replication need to set the jvm flag `-Dcassandra.testtag=enable_transient_replication` (otherwise CC fails to start as this is not a supported feature).
    
    Includes a fix for cql_tracing_test.py
    
     (Rebase of commit 645291b35e36d25904a8d59aa268c11109dd70f9)

--- transient_replication_ring_test.py
+++ transient_replication_ring_test.py
@@ -10,6 +10,7 @@ from ccmlib.node import NODE_WAIT_TIMEOUT_IN_SECS
 
 from dtest import Tester
 from tools.assertions import (assert_all)
+from transient_replication_test import TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG
 
 from flaky import flaky
 
@@ -120,8 +121,8 @@ class TestTransientReplicationRing(Tester):
         logger.debug("CLUSTER INSTALL DIR: ")
         logger.debug(self.cluster.get_install_dir())
         self.cluster.populate(3, tokens=self.tokens, debug=True, install_byteman=True)
-        # self.cluster.populate(3, debug=True, install_byteman=True)
-        self.cluster.start(jvm_args=['-Dcassandra.enable_nodelocal_queries=true'],
+        # converged core need this sys prop set, otherwise it fails startup as transient rep is not supported
+        self.cluster.start(jvm_args=['-Dcassandra.enable_nodelocal_queries=true', TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG],
                            timeout=DEFAULT_CLUSTER_WAIT_TIMEOUT_IN_SECS * 4)
 
         # enable shared memory
@@ -185,7 +186,7 @@ class TestTransientReplicationRing(Tester):
             self.insert_row(i, i, i, main_session)
 
         # we don't need to wait for other nodes, see CASSANDRA-18660
-        nodes[1].start(wait_for_binary_proto=True, wait_other_notice=False)
+        nodes[1].start(wait_for_binary_proto=True, wait_other_notice=False, jvm_args=[TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG])
 
         sessions = [self.exclusive_cql_connection(node) for node in [self.node1, self.node2, self.node3]]
 
@@ -199,7 +200,7 @@ class TestTransientReplicationRing(Tester):
         node4 = new_node(self.cluster, bootstrap=True, token='00040')
         patch_start(node4)
         nodes.append(node4)
-        node4.start(wait_for_binary_proto=True)
+        node4.start(wait_for_binary_proto=True, jvm_args=[TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG])
 
         expected.append(gen_expected(range(11, 20, 2), range(21, 40)))
         sessions.append(self.exclusive_cql_connection(node4))
@@ -238,7 +239,8 @@ class TestTransientReplicationRing(Tester):
         """Helper method to run a move test cycle"""
         node4 = new_node(self.cluster, bootstrap=True, token='00040')
         patch_start(node4)
-        node4.start(wait_for_binary_proto=NODE_WAIT_TIMEOUT_IN_SECS * 2)
+        # converged core need this sys prop set, otherwise it fails startup as transient rep is not supported
+        node4.start(wait_for_binary_proto=NODE_WAIT_TIMEOUT_IN_SECS * 2, jvm_args=[TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG])
         main_session = self.patient_cql_connection(self.node1)
         nodes = [self.node1, self.node2, self.node3, node4]
 
@@ -257,7 +259,7 @@ class TestTransientReplicationRing(Tester):
             logger.debug("Inserting " + str(i))
             self.insert_row(i, i, i, main_session)
 
-        nodes[1].start(wait_for_binary_proto=True)
+        nodes[1].start(wait_for_binary_proto=True, jvm_args=[TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG])
         sessions = [self.exclusive_cql_connection(node) for node in [self.node1, self.node2, self.node3, node4]]
 
         expected = [gen_expected(range(0, 11), range(31, 40)),
@@ -346,7 +348,7 @@ class TestTransientReplicationRing(Tester):
         """Test decommissioning a node correctly streams out all the data"""
         node4 = new_node(self.cluster, bootstrap=True, token='00040')
         patch_start(node4)
-        node4.start(wait_for_binary_proto=True)
+        node4.start(wait_for_binary_proto=True, jvm_args=[TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG])
         main_session = self.patient_cql_connection(self.node1)
         nodes = [self.node1, self.node2, self.node3, node4]
 
@@ -365,7 +367,7 @@ class TestTransientReplicationRing(Tester):
             logger.debug("Inserting " + str(i))
             self.insert_row(i, i, i, main_session)
 
-        nodes[1].start(wait_for_binary_proto=True)
+        nodes[1].start(wait_for_binary_proto=True, jvm_args=[TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG])
         sessions = [self.exclusive_cql_connection(node) for node in [self.node1, self.node2, self.node3, node4]]
 
         expected = [gen_expected(range(0, 11), range(31, 40)),
@@ -405,7 +407,7 @@ class TestTransientReplicationRing(Tester):
         """Test  a mix of ring change operations across a mix of transient and repaired/unrepaired data"""
         node4 = new_node(self.cluster, bootstrap=True, token='00040')
         patch_start(node4)
-        node4.start(wait_for_binary_proto=True)
+        node4.start(wait_for_binary_proto=True, jvm_args=[TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG])
         main_session = self.patient_cql_connection(self.node1)
         nodes = [self.node1, self.node2, self.node3]
 
@@ -455,7 +457,9 @@ class TestTransientReplicationRing(Tester):
         self.cluster.add(self.node2, False, data_center='datacenter1')
         jvm_args = ["-Dcassandra.replace_address=%s" % replacement_address,
                     "-Dcassandra.ring_delay_ms=10000",
-                    "-Dcassandra.broadcast_interval_ms=10000"]
+                    "-Dcassandra.broadcast_interval_ms=10000",
+                    # converged core need this sys prop set, otherwise it fails startup as transient rep is not supported
+                    TRANSIENT_REPLICATION_TEST_ENABLED_JVM_ARG]
         self.node2.start(jvm_args=jvm_args, wait_for_binary_proto=True)
 
         sessions = [self.exclusive_cql_connection(node) for node in [self.node1, self.node2, self.node3]]
