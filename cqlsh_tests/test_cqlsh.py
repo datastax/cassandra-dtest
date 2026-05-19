dad431a29ca4563fcac37d49c299b28f91d3bbc7
Brandon Williams <brandonwilliams@apache.org>
2026-05-19 14:10:51 -0500

    Fix cqlsh unicode invalid request expectation

--- cqlsh_tests/test_cqlsh.py
+++ cqlsh_tests/test_cqlsh.py
@@ -602,7 +602,7 @@ UPDATE varcharmaptable SET varcharvarintmap['Vitrum edere possum, mihi non nocet
             # See CASSANDRA-15985 for more details
             err = node1.run_cqlsh(cmds=cmd, cqlsh_options=options).stderr
 
-        if self.is_cc5() or self.is_cc4():
+        if self.supports_cc4_guardrails():
             assert "Keyspace name must not be empty, more than 222 characters long, or contain non-alphanumeric-underscore characters (got 'ä')" in err
         elif self.cluster.version() >= LooseVersion('4.0'):
             assert 'Keyspace name must not be empty and must contain alphanumeric or underscore characters only (got "ä")' in err
